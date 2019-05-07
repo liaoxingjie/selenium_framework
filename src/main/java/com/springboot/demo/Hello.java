a56f8b331f2c960255e5d3f661346f9c45ce59b0
liaoxingjie <liaoxingjie@sunlands.com>
2019-05-07 13:41:40 +0800

    spring boot demo+ jacoco demo

--- src/main/java/com/springboot/demo/Hello.java
+++ src/main/java/com/springboot/demo/Hello.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(){
-        //测试
-        if (true){
-            return "IF True-------branch";
-        }else {
-            return "ELSE----------branch";
+    public String hello(int a){
+        //测试jacoco对分支的覆盖
+        if (a == 1){
+            return "Hello a = 1";
+        }else if(a == 2){
+            return "Hello a = 2";
+        } else {
+            return "I don't know a = ?";
         }
 
     }
